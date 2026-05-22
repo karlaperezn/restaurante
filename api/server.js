d65bf36c7fd9e10cb459490e3242520dc447a710
MekaKoatl <carlos-raf@outlook.com>
2026-05-22 18:57:04 +0200

    Modificaciones a endpoints

---
+++ api/server.js
@@ -0,0 +1,41 @@
+import express from "express";
+import cors from "cors";
+import { MongoClient } from "mongodb";
+
+const app = express();
+app.use(cors());
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+let db;
+
+async function start() {
+  try {
+    const client = await MongoClient.connect(uri);
+    db = client.db("restaurante");
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  } catch (err) {
+    console.error("Mongo error:", err);
+  }
+}
+start();
+
+//todos los menus
+app.get("/api/menus", async (req, res) => {
+  const menus = await db.collection("menus").find().toArray();
+
+  res.send(menus);
+});
+
+
+//nuevo menu
+app.post("/api/nuevoMenu", async (req, res) => {
+  const propiedades = req.body;
+  const menu = await db.collection("menu").insertOne({ propiedades });
+
+   res.send({ message: "Menú añadido", data: menu });
+});
